80a5fe5a54e688a79df8e487dcf2699b22988095
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-07-21 15:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 <<<<<<< HEAD
 const companyName = 'Scuber';
-let companyName = 'Scuber';
+
 let mostProfitableNeighborhood='Chelsea';
 
 let companyCeo='Susan Smith';
